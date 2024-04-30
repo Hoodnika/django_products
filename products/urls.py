1b99fc0048b65e61b27e72ddc5c6456873c65ad4
Hoodnika <Hoodnika213@gmail.com>
2024-04-30 11:26:51 +0500

    add navigate and sort by category, moving to a specific product

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ from products.views import home, products_to_add, product_detail
 app_name = ProductsConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('products_to_add/', products_to_add, name='products_to_add'),
+    path('home/', home, name='home'),
+    path('products_to_add/<int:category_pk>', products_to_add, name='products_to_add'),
     path('product_detail/<int:pk>/', product_detail, name='product_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -11,15 +11,16 @@ def home(request):
     return render(request, 'products/home.html', context)
 
 
-def products_to_add(request):
+def products_to_add(request, category_pk: int):
     context = {
-        "products_list": Product.objects.all(),
+        "products_list": Product.objects.all().filter(category=category_pk),
     }
     return render(request, 'products/products_to_add.html', context)
 
 
-def product_detail(request, pk):
+def product_detail(request, pk: int):
     context = {
-        "object": get_object_or_404(Product, pk=pk),
+        # "object": get_object_or_404(Product, pk=pk),
+        'object': Product.objects.get(pk=pk),
     }
     return render(request, 'products/product_detail.html', context)
